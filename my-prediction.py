0fda752d2d426b70a1127a034ff024021cb74704
Bibushan Sai <bibushansai@gmail.com>
2024-02-25 22:17:55 +0530

    add script to run it with user's inputs

---
+++ my-prediction.py
@@ -0,0 +1,158 @@
+# from tqdm import tqdm
+import os
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import pickle as pkl
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision.transforms as transforms
+
+
+from data_loader import get_loader
+# from data_loader import my_get_loader
+from models import VqaModel
+
+def loadPkl(file_path, delimiter='\n'):
+	with open(file_path, "rb") as f:
+		data = pkl.load(f)
+	return data
+def getWord2IdxMap(idx2word):
+	word2idx = {}
+	for idx, word in enumerate(idx2word):
+		word2idx[word]=idx
+	return word2idx	
+
+
+def loadTxtFile(file_path, delimiter='\n'):
+	""" Function to load text files as numpy arrays
+	Inputs: 
+	    file_path: path to input file
+	    delimter: (optional) delimiter to be used. "\n" by default
+	Return Value: An array containing data from the text file, seperated by delimiter
+	"""
+	with open(file_path, "r") as f:
+		arr = f.read()
+	return arr.split(delimiter)
+
+def readImage(path):
+	print("Print from readImage before return")
+	return cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+
+def showImage(img):
+	plt.imshow(img)
+	plt.savefig('my_figure.png')  # Save the figure to a file
+	#plt.show()
+	
+
+def resizeImage(img, target_size=(224,224)):
+	return cv2.resize(img, target_size, interpolation = cv2.INTER_AREA)
+
+def normalizeImage(img, mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225]):
+	img = img / 255
+	img = img.astype("float32")
+
+	r = img[:,:,0]
+	g = img[:,:,1]
+	b = img[:,:,2]
+
+	x = (r-mean[0])/std[0]
+	y = (g-mean[1])/std[1]
+	z = (b-mean[2])/std[2]
+	normalized_img = np.stack([x,y,z], axis=2)
+	return normalized_img
+
+def transformImage(img, mean=[0.485,0.456,0.406], std=[0.229,0.224,0.225], target_size=(224,224)):
+	resized_img = resizeImage(img, target_size)
+	normalized_img = normalizeImage(resized_img, mean, std)
+	transposed_img = np.transpose(normalized_img, axes=[2,0,1])
+	m,n,p = transposed_img.shape
+	transformed_img = transposed_img.reshape(1,m,n,p)
+	return torch.from_numpy(transformed_img)
+
+
+def encodeQuestion(qst_str, qst_word2idx, max_qst_length):
+	qst_str = qst_str.strip()
+	qst_words = qst_str.split(" ")
+	count=0
+
+	my_question = []
+	for qst_word in (qst_words):
+		my_question.append(qst_word2idx[qst_word])
+		count+=1
+
+	while count < max_qst_length:
+		my_question.append(0)
+		count+=1
+
+	my_question = np.asarray(my_question)
+	my_question = my_question.reshape((1,-1))
+	qst_tensor = torch.from_numpy(my_question)
+
+	return qst_tensor	
+
+def getPrediction(img, qst_str):
+	### Make Predictions ###
+	transformed_img = transformImage(img)
+	qst_encoded = encodeQuestion(qst_str, qst_word2idx, max_qst_length)
+	my_output = model(transformed_img, qst_encoded)
+	_, my_pred = torch.max(my_output, 1)
+	predicted_answer = ans_idx2ans[my_pred]
+	print("Question was: ", qst_str)
+	print("Predicted Answer is:", predicted_answer)
+	showImage(img)
+
+if __name__ == "__main__":
+	### Define global variables ###
+	#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+	device = 'cpu'
+	print("device: ", device)
+
+	qst_idx2word = loadTxtFile("./datasets/vocab_questions.txt", delimiter="\n")
+	qst_idx2word = np.asarray(qst_idx2word)[:-1]
+	ans_idx2ans = loadTxtFile("./datasets/vocab_answers.txt", delimiter="\n")
+	ans_idx2ans = np.asarray(ans_idx2ans)[:-1]
+	qst_word2idx = getWord2IdxMap(qst_idx2word)
+	# qst_word2idx = loadPkl("../data_dump/vqa_binary_processed/mapping_questions_word2idx.pkl")
+
+
+	qst_vocab_size = len(qst_idx2word)
+	ans_vocab_size = len(ans_idx2ans)
+	max_qst_length = 30
+	ans_unk_idx = 0
+
+	# qst_vocab_size = data_loader['train'].dataset.qst_vocab.vocab_size
+	# ans_vocab_size = data_loader['train'].dataset.ans_vocab.vocab_size
+	# ans_unk_idx = data_loader['train'].dataset.ans_vocab.unk2idx
+
+
+	embed_size = 1024
+	word_embed_size = 300
+	num_layers = 2
+	hidden_size = 512
+
+	### Define and load model ###
+
+	model = VqaModel(
+	    embed_size=embed_size,
+	    qst_vocab_size=qst_vocab_size,
+	    ans_vocab_size=ans_vocab_size,
+	    word_embed_size=word_embed_size,
+	    num_layers=num_layers,
+	    hidden_size=hidden_size).to(device)
+
+	model.load_state_dict(torch.load("./models/model-epoch-03.ckpt", map_location=torch.device(device))['state_dict'])
+
+	
+	print("Printing model architecture:\n", model.eval())
+
+	# ### Read / Get input ###
+	img = readImage("./datasets/Images/test2015/COCO_test2015_000000000019.jpg")
+	print("Print after readImage")
+	#qst_str = input("Enter a question: ")
+	qst_str = "what is the animal in the image ?"
+	getPrediction(img, qst_str)
--- run_local.py
+++ run_local.py
@@ -4,9 +4,16 @@ import argparse
 import numpy as np
 from PIL import Image
 from torchvision import transforms
-from models import VqaModel
-from data_loader import VQAImageProcessor
 from utils import text_helper
+from utils.resize_images import resize_image
+from utils.make_vacabs_for_questions_answers import make_vocab_questions
+from utils.make_vacabs_for_questions_answers import make_vocab_answers
+from utils.build_vqa_inputs import vqa_processing
+from models import VqaModel
+from data_loader import get_loader
+
+
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -16,19 +23,68 @@ def load_image(image_path, transform):
     return image.to(device)
 
 def main(args):
+    # resize the input image
+    # Path to the input image
+    image_path =  args.image_path ##r"E:\Research\vqa-ssm\datasets\Images\train2014\COCO_train2014_000000000025.jpg"
+
+    # Desired size after resizing
+    image_size = (224, 224)
+
+    print("Image path:", image_path)
+
+    # Open the image
+    image = Image.open(image_path)
+
+    # Resize the image
+    resized_image = resize_image(image, image_size)
+
+    # Save or use the resized image as needed
+    resized_image.save("resized_image.jpg")
+
+
+    # Make vocabularies for questions and answers
+#    make_vocab_questions(args.questions_dir)
+ #   make_vocab_answers(args.annotations_dir, args.n_answers)
+
+    # Build VQA inputs
+#    vqa_processing(args.resized_images_dir, args.annotations_file, args.questions_file, args.vocab_answers_file, 'test-dev2015')
+
+
+    data_loader = get_loader(
+        input_dir=args.input_dir,
+        input_vqa_train='train.npy',
+        input_vqa_valid='valid.npy',
+        max_qst_length=args.max_qst_length,
+        max_num_ans=args.max_num_ans,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers)
+
+    qst_vocab_size = data_loader['train'].dataset.qst_vocab.vocab_size
+    ans_vocab_size = data_loader['train'].dataset.ans_vocab.vocab_size
+    ans_unk_idx = data_loader['train'].dataset.ans_vocab.unk2idx
+
     # Load the trained model
     model = VqaModel(embed_size=args.embed_size,
-                     qst_vocab_size=args.qst_vocab_size,
-                     ans_vocab_size=args.ans_vocab_size,
+                     qst_vocab_size=qst_vocab_size,
+                     #qst_vocab_size=text_helper.VocabDict(args.input_dir+'/vocab_questions.txt'),
+
+                     ans_vocab_size=ans_vocab_size,
+                     #ans_vocab_size=text_helper.VocabDict(args.input_dir+'/vocab_answers.txt'),
                      word_embed_size=args.word_embed_size,
                      num_layers=args.num_layers,
                      hidden_size=args.hidden_size)
+    
+    print("Pre load_state_dict")
+    
+    
+    for param_tensor in model.state_dict():
+        print(param_tensor, "\t", model.state_dict()[param_tensor].size())
+
     model.load_state_dict(torch.load(args.model_path, map_location=device))
     model.to(device)
     model.eval()
 
-    # Load vocabulary and other necessary data
-    processor = VQAImageProcessor(args.vocab_path)
+    print("Model has been loaded")  
     
     # Image preprocessing
     transform = transforms.Compose([
@@ -39,20 +95,23 @@ def main(args):
 
     # Process image and question
     image = load_image(args.image_path, transform)
-    question = processor.process_question(args.question)
+    question = text_helper.tokenize(args.question)
+
 
     # Get the answer
     with torch.no_grad():
         output = model(image, question)
         _, answer_idx = torch.max(output, dim=1)
-        answer = processor.idx_to_answer(answer_idx.item())
+        answer = text_helper.idx_to_answer(answer_idx.item())
 
     print("Predicted Answer:", answer)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='./datasets',
+                        help='input directory for visual question answering.')
     parser.add_argument('--model_path', type=str, required=True, help='Path to the trained model file')
-    parser.add_argument('--vocab_path', type=str, required=True, help='Path to vocabulary file')
+    parser.add_argument('--vocab_path', type=str, default="./datasets/vocab_questions.txt", help='Path to vocabulary file')
     parser.add_argument('--image_path', type=str, required=True, help='Path to the input image')
     parser.add_argument('--question', type=str, required=True, help='Question about the image')
     parser.add_argument('--embed_size', type=int, default=1024, help='Embedding size')
@@ -61,6 +120,15 @@ if __name__ == "__main__":
     parser.add_argument('--word_embed_size', type=int, default=300, help='Word embedding size')
     parser.add_argument('--num_layers', type=int, default=2, help='Number of LSTM layers')
     parser.add_argument('--hidden_size', type=int, default=512, help='Hidden size of LSTM')
+    parser.add_argument('--max_qst_length', type=int, default=30,
+                        help='maximum length of question. \
+                              the length in the VQA dataset = 26.')
+    parser.add_argument('--max_num_ans', type=int, default=10,
+                        help='maximum number of answers.')
+    parser.add_argument('--batch_size', type=int, default=256,
+                        help='batch_size.')
+    parser.add_argument('--num_workers', type=int, default=8,
+                        help='number of processes working on cpu.')
     args = parser.parse_args()
 
     main(args)
